c3b59b99988ea598ebdf51b2b278354d08676647
ferho20u12 <ferho20u12@gmail.com>
2024-10-07 23:47:59 -0600

    updated images

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-    alert('¡Bienvenido a la Fan Page de Post Malone!');
-});
+window.onload = function() {
+    var audio = document.getElementById("miCancion");
+    audio.play();
+};
+window.onload = function() {
+    let images = document.querySelectorAll('.image');
+    let index = 0;
+
+    function showImages() {
+        // Oculta todas las imágenes
+        images.forEach(img => img.style.opacity = 0);
+
+        // Muestra las imágenes de acuerdo al tamaño de pantalla
+        if (window.innerWidth > 768) {
+            // Para escritorio, mostrar dos imágenes
+            images[index].style.opacity = 1;
+            if (images[index + 1]) {
+                images[index + 1].style.opacity = 1;
+            }
+        } else {
+            // Para móvil, mostrar solo una imagen
+            images[index].style.opacity = 1;
+        }
+
+        // Actualiza el índice
+        index = (index + (window.innerWidth > 768 ? 2 : 1)) % images.length;
+    }
+
+    // Llama a la función showImages cada 3 segundos
+    setInterval(showImages, 4000);
+
+    // Inicia mostrando las primeras imágenes
+    showImages();
+};
