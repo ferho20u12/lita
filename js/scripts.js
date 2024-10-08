870f87444051b1bee2464d6919108436559fce90
ferho20u12 <ferho20u12@gmail.com>
2024-10-08 01:48:07 -0600

    fixed images

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,6 @@ window.onload = function() {
     var audio = document.getElementById("miCancion");
     audio.play();
 };
-let currentSlide = 0;
-
-function showSlide(index) {
-    const slides = document.querySelectorAll('.carousel-item');
-    const totalSlides = slides.length;
-
-    // Asegúrate de que el índice esté en el rango correcto
-    currentSlide = (index + totalSlides) % totalSlides; // Ciclamos el índice
-    const offset = -currentSlide * 100; // Calculamos el desplazamiento
-    const carouselInner = document.querySelector('.carousel-inner');
-    carouselInner.style.transform = `translateX(${offset}%)`; // Desplazamos el carrusel
-}
-
-function moveSlide(direction) {
-    showSlide(currentSlide + direction);
-}
 
 // Inicializa el carrusel mostrando la primera imagen
 showSlide(currentSlide);
