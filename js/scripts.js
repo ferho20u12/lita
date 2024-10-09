17853efe5262a22c6d418a7fba3eadd30fa52cdb
ferho20u12 <ferho20u12@gmail.com>
2024-10-08 23:34:54 -0600

    fixed

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,4 @@ window.onload = function() {
 // Inicializa el carrusel mostrando la primera imagen
 showSlide(currentSlide);
 
-const items = document.querySelectorAll('.favorite-item');
-let currentIndex = 0;
-
-function showNextItem() {
-    items[currentIndex].classList.remove('active');
-    currentIndex = (currentIndex + 1) % items.length;
-    items[currentIndex].classList.add('active');
-}
-
 setInterval(showNextItem, 4000);
